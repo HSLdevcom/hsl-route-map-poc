bcbfae9b4bfb55a19d9a3340350f14cd3c094daf
Hannes Junnila <hannes.junnila@gmail.com>
2017-11-06 01:23:23 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var fs = require("fs");
+var express = require("express");
+
+var style = require("hsl-map-style").generateStyle({
+    sourcesUrl: "http://api.digitransit.fi/map/v1/",
+    glyphsUrl: "http://kartat.hsl.fi/",
+    components: {
+        text_fisv: { enabled: true },
+        routes: { enabled: true },
+        stops: { enabled: true },
+        citybikes: { enabled: true },
+        icons: { enabled: true },
+        print: { enabled: false },
+        municipal_borders: { enabled: true },
+    }
+});
+
+var app = express();
+
+app.get("/", function (req, res) {
+    res.redirect("/index.html");
+});
+
+app.get("/index.html", function (req, res) {
+    res.set("Content-Type", "text/html");
+    var index = fs.readFileSync("index.html", "utf8");
+    res.send(index);
+});
+
+app.get("/style.json", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send(style);
+});
+
+app.listen(3000, function () {
+    console.log("Listening at localhost:3000");
+});
